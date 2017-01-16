f708bde5f017751bce2f7c6d84dcb3a539c90e91
Björn Hjortsten <bjornhjortsten@gmail.com>
2017-01-16 23:22:46 +0100

    Added a message for when no players are connected to the listplayers command.
    Better then no output.

--- src/Console/ListPlayersCommand.php
+++ src/Console/ListPlayersCommand.php
@@ -7,26 +7,30 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ListPlayersCommand extends ConfigAwareCommand {
-	
+
 	/** @var  Rcon */
 	protected $rcon;
-	
+
 	protected function configure() {
 		$this
 			->setName('listplayers')
 			->setDescription('List the current players')
 		;
 	}
-	
+
 	protected function initialize(InputInterface $input, OutputInterface $output) {
 		parent::initialize($input, $output);
 		$this->rcon = new Rcon($this->config);
 	}
-	
+
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		//$output->writeln($this->rcon->listPlayers());
-		foreach ($this->rcon->listPlayers() as $player) {
-			$output->writeln($player);
+		$players = $this->rcon->listPlayers();
+		if (!empty($players)) {
+			foreach ($players as $player) {
+				$output->writeln($player);
+			}
+		} else {
+			$output->writeln('No players connected');
 		}
 	}
 }
